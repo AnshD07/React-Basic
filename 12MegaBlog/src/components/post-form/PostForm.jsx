4e1cf2b33090996f2884029d37a2fcdc3d404c60
AnshD07 <dholakiyaansh1707@gmail.com>
2024-10-10 08:57:04 +0530

    9 oct 2024

---
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import { Button, Input, Select, RTE } from "../index"
+import appwriteService from '../../appwrite/config'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function PostForm({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || "",  //! if there is post then title take or else empty
+            slug: post?.slug || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        }
+    });
+
+    const navigate = useNavigate()
+    const userData = useSelector((state) => state.auth.userData)
+
+    const submit = async (data) => {
+        if(post){  //! if there is post then update it
+            const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+        }
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PostForm
+
+
+
+
+
+
